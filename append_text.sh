519a0ef854a2f45f6c24070d0925d2caf65712b2
trimethylpurine <jonathan_tay@live.com>
2016-07-13 15:18:19 -0400

    Update append_text.sh

--- append_text.sh
+++ append_text.sh
@@ -1,15 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 
-subj=(001 002 003 004)
-run=(1 2)
+for i in {001..004}
+do
+	for j in 1 2
+	do
+		cd /Users/labuser/Documents/Jonathan_gapclass/highres_data/york/behaviour/onsets_imagine/${i}/imagine${j}
+		cp e1.txt /Users/labuser/Documents/Jonathan_gapclass/highres_data/york/behaviour/onsets_imagine/${i}/imagine${j}/${i}_imagine${j}_stacked.txt
+		cat e2.txt >> ${i}_imagine${j}_stacked.txt | cat e3.txt >> ${i}_imagine${j}_stacked.txt | cat e4.txt >> ${i}_imagine${j}_stacked.txt
+		echo "Created stacked onset file for subject ${i} imagine${j}!"
 
-for n in $subj; do
-	for r in $run; do
-
-		cd /Users/labuser/Documents/Jonathan_gapclass/highres_data/york/behaviour/onsets_imagine/${subj}/imagine${run}
-		cp e1.txt /Users/labuser/Documents/Jonathan_gapclass/highres_data/york/behaviour/onsets_imagine/${subj}/imagine${run}/${subj}_imagine${run}_stacked.txt
-		cat e2.txt >> ${subj}_imagine${run}_stacked.txt | cat e3.txt >> ${subj}_imagine${run}_stacked.txt | cat e4.txt >> ${subj}_imagine${run}_stacked.txt
-		echo "Created stacked onset file for subject ${subj} imagine${run}!"
-
-done
+	done
 done
