2cb9d24fa9e68d8576a5e50c03576e94ae0f1994
trimethylpurine <jonathan_tay@live.com>
2016-08-12 17:26:50 -0400

    Update create_onsets.m

--- create_onsets.m
+++ create_onsets.m
@@ -1,8 +1,13 @@
-excel(:,1) = [ 1; 4; 2; 3; 1; 3];
-excel(:,2) = [1;1;1;0;0;1];
+%excel(:,1) = [ 1; 4; 2; 3; 1; 3];
+%excel(:,2) = [1;1;1;0;0;1];
+
+%creates a matrix called excel; column 1 is the A range on the spreadsheet, while column 2 is the D range)
+excel(:,1) = xlsread('test_excel.xlsx','','A2:A13')
+excel(:,2) = xlsread('test_excel.xlsx','','D2:D13')
 
 masterData = excel;
 
+%Boolean that sets column 2 of master data to true or false
 index1 = masterData(:,2) == 1;
 index0 = masterData(:,2) == 0;
 
